ac2867b2e08fa07c8e3026394e9c41e5de58e8ab
dsdrajlin <dsdrajlin@gmail.com>
2023-10-23 13:39:18 -0300

    Se crea sentiment_analysis en la API

--- main.py
+++ main.py
@@ -6,6 +6,21 @@ app = FastAPI()
 
 @app.get("/PTG/{genero}")
 def PlayTimeGenre( genero : str ):
+    # Crear un dataframe donde las filas son géneros y las columnas años.
     df = pd.read_csv("./Datasets_API/PlayTimeGenre.csv")
+
+    # Encontrar la columna con el mayor valor para la fila género.
     anio = int(df.loc[df[genero].idxmax()]["year"])
     return {f"Año de lanzamiento con más horas jugadas para Género {genero}" : anio}
+
+@app.get("/SA/{anio}")
+def sentiment_analysis( anio : int ):
+    # Crear un dataframe donde las filas son años y las columnas análisis de sentimiento.
+    df = pd.read_csv("./Datasets_API/sentiment_analysis.csv")
+
+    # Encontrar los valores de sentimiento para el año pasado como parámetro.
+    negativo = df.loc[df.year == anio, "0"].item()
+    neutral = df.loc[df.year == anio, "1"].item()
+    positivo = df.loc[df.year == anio, "2"].item()
+
+    return {"Negative" : negativo, "Neutral" : neutral, "Positive" : positivo}
